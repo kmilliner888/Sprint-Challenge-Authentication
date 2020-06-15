2c795e2f24029cef33a87e1f045b0c0f96b83394
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-06-14 22:07:52 -0400

    added users model, middleware, auth router, able to get jokes

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token= req.headers.authorization;
+  
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({message: "whoops, sorry friend"});
+      } else {
+        req.decodedJwt = decodedToken;
+        console.log(req.decodedJwt);
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
+  
 };
